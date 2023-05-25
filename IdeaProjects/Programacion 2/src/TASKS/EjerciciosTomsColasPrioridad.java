964db38c2c41530da5a3eec4fb0129b5d4f1bcad
GabrielAran <gabitoaran@gmail.com>
2023-05-25 18:52:49 -0300

    full colas prioridades

--- IdeaProjects/Programacion 2/src/TASKS/EjerciciosTomsColasPrioridad.java
+++ IdeaProjects/Programacion 2/src/TASKS/EjerciciosTomsColasPrioridad.java
@@ -1,22 +1,59 @@
 package TASKS;
 import Clase_02.Colas;
-import Clase_02.ColasConPrioridad;
+import NodosCosas.PriorityQueue;
 
 public class EjerciciosTomsColasPrioridad {
     public static void main(String[] args){
         //COLAS CON PRIORIDAD
-
+        valsYprios(crearColasPrioridad());
+        mostrarColaPrioridad(crearColasPrioridad());
+        copiarColaPrioridad(crearColasPrioridad());
     }
 
-    public void Colap2Colas(ColasConPrioridad priorityQueue) { //CONVIERTE UNA COLA CON PRIORIDAD EN 2 COLAS, UNA DE VALORES Y UNA DE PRIORIDADES
-
+    public static PriorityQueue crearColasPrioridad(){
+        PriorityQueue colaPrio = new PriorityQueue();
+        colaPrio.add(45, 1);
+        colaPrio.add(55, 2);
+        colaPrio.add(65, 3);
+        return colaPrio;
+    }
+    public static void valsYprios(PriorityQueue priorityQueue) { //CONVIERTE UNA COLA CON PRIORIDAD EN 2 COLAS, UNA DE VALORES Y UNA DE PRIORIDADES
+        Colas valores = new Colas();
+        Colas prioridades = new Colas();
+        while (!priorityQueue.isEmpty()){
+            valores.acolar(priorityQueue.getFirstValue()); //acolo valores
+            prioridades.acolar(priorityQueue.getFirstPriority()); //acolo prioridades
+            priorityQueue.remove(); //desacolo
+        }
+        /* ESTO ES PA VER Q FUNCIONE -->
+        while (!valores.estaVacio()){
+            System.out.println(valores.getPrimero());
+            System.out.println(prioridades.getPrimero());
+            valores.desacolar();
+            prioridades.desacolar();
+        } */
     }
 
-    public static void mostrarColaPrioridad() { //PRINTEA EL CONTENIDO DE UNA COLA CON PRIORIDAD EN EL FORMATO "ELEMENTO(PRIORIDAD:)"
-
+    public static void mostrarColaPrioridad(PriorityQueue full) { //PRINTEA EL CONTENIDO DE UNA COLA CON PRIORIDAD EN EL FORMATO "ELEMENTO(PRIORIDAD:)"
+        while (!full.isEmpty()){
+            System.out.println(full.getFirstValue() + "(" + full.getFirstPriority() + ")");
+            full.remove();
+        }
     }
 
-    public static ColasConPrioridad copiarColaPrioridad() { //GENERA UNA COPIA DE UNA COLA CON PRIORIDAD
-
+    public static void copiarColaPrioridad(PriorityQueue cola) { //GENERA UNA COPIA DE UNA COLA CON PRIORIDAD
+        PriorityQueue copia = new PriorityQueue();
+        PriorityQueue aux = new PriorityQueue();
+        while (!cola.isEmpty()){
+            copia.add(cola.getFirstValue(), cola.getFirstPriority());
+            aux.add(cola.getFirstValue(), cola.getFirstPriority());
+            cola.remove();
+        }
+        while (!aux.isEmpty()){
+            cola.add(aux.getFirstValue(), aux.getFirstPriority());
+            aux.remove();
+        }
+        // mostrarColaPrioridad(cola); chequeamos q funione
+        // mostrarColaPrioridad(copia); chequeamos q funione
     }
 }
